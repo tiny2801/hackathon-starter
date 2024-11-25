4b795cd5a401db865ccdf55be07d73dd336972a4
tiny2801 <77006919+tiny2801@users.noreply.github.com>
2024-11-25 22:38:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -275,8 +275,7 @@ app.listen(PORT, () => {
 
 
   if (!BASE_URL.startsWith('http://localhost')) {
-    console.log(The BASE_URL env variable is set to ${BASE_URL}. If you directly test the application through http://localhost:${app.get('port')} instead of the BASE_URL, it may cause a CSRF mismatch or an Oauth authentication failur. To avoid the issues, change the BASE_URL or configure your proxy to match it.\n);
-  } else if (app.get('port') !== port) {
+    console.log(`The BASE_URL env variable is set to ${BASE_URL}. If you directly test the application through http://localhost:${app.get('port')} instead of the BASE_URL, it may cause a CSRF mismatch or an Oauth authentication failure. To avoid the issues, change the BASE_URL or configure your proxy to match it.\n`);
     console.warn(WARNING: The BASE_URL environment variable and the App have a port mismatch. If you plan to view the app in your browser using the localhost address, you may need to adjust one of the ports to make them match. BASE_URL: ${BASE_URL}\n);
   }
 
